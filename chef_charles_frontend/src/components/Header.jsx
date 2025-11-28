fed8545e719ffcab8ce00b9c86e14bb6d54e7d6f
Elizbeh <105496336+Elizbeh@users.noreply.github.com>
2025-11-28 15:57:26 +0100

    Add Google Translate widget to header for multilingual support

--- chef_charles_frontend/src/components/Header.jsx
+++ chef_charles_frontend/src/components/Header.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Header.css'
 
+import Logo from "../assets/logo.png"
+
+
 const Header = () => {
+
+  useEffect(() =>{
+    if(!window.googleTranslateElementInit) {
+      window.googleTranslateElementInit = () => {
+        new window.google.translate.TranslateElement(
+          {pageLanguage: "en"},
+          "google_translate_element"
+        );
+      };
+
+      const script = document.createElement("script");
+      script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  }, [])
   return (
     <header className='header'>
-        <img className='logo' src="https://image.similarpng.com/file/similarpng/very-thumbnail/2021/06/Chef-icon-illustration-on-transparent-background-PNG.png" alt="lLogo" />
-        <h1 className='app-name'>Chef Charles</h1>
+        <div className='header-container'>
+          <img className='logo' src={Logo} alt="Logo" />
+          <h1 className='app-name'>Chef Charles</h1>
+
+        </div>
+        {/* Google Translate widget */}
+        <div id="google_translate_element"></div>
     </header>
   )
 }
